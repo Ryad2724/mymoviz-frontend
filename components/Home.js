00d0084735dc27420c09c0ec5828221b7d0544fc
mentizi <mentizi.ryad@gmail.com>
2024-04-24 14:55:56 +0200

    frontv3

--- components/Home.js
+++ components/Home.js
@@ -23,7 +23,7 @@ function Home() {
  
     
   useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('http://mymoviz-backend-gilt-alpha.vercel.app/movies')
     .then(response=>response.json())
     .then(data=> {
       console.log(data)
